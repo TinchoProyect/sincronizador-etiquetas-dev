17b2548df6ab09455ad7fd75a3352585d148adb5
TinchoProyect <miserrano75@gmail.com>
2025-08-09 22:59:34 -0300

    Sincro de clientes incorporada

--- src/actualizaArticulos/sincronizar-etiquetas.bat
+++ src/actualizaArticulos/sincronizar-etiquetas.bat
@@ -1,3 +1,16 @@
-cd %USERPROFILE%\Documents\sincronizador-etiquetas-copia
-node src\actualizaArticulos\syncEtiquetas.js
+@echo off
+setlocal
+
+REM Ir a la carpeta donde está este .bat (src\actualizaArticulos)
+pushd "%~dp0"
+
+echo Sincronizando articulos...
+node syncEtiquetas.js
+
+echo.
+echo Sincronizando clientes...
+node ..\actualizaClientes\syncClientes.js
+
+popd
+endlocal
 pause
