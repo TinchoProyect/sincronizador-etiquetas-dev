1378a3ab10dd5aad766e5e43a079bc357d82f8cc
TinchoProyect <miserrano75@gmail.com>
2025-08-29 11:16:11 -0300

    Nueva tabla de precios

--- src/actualizaArticulos/sincronizar-etiquetas.bat
+++ src/actualizaArticulos/sincronizar-etiquetas.bat
@@ -11,6 +11,13 @@ echo.
 echo Sincronizando clientes...
 node ..\actualizaClientes\syncClientes.js
 
+echo.
+echo Sincronizando precios...
+set "LOMASOFT_ARTICULOS_URL=https://api.lamdaser.com/precios"
+REM Si tu endpoint real es /api/precios, usar:
+REM set "LOMASOFT_ARTICULOS_URL=https://api.lamdaser.com/api/precios"
+node ..\actualizaPrecios\syncPrecios.js
+
 popd
 endlocal
 pause
